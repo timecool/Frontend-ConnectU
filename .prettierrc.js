d98c36efa61b2f7f765c076cd07fde860176ef94
Vincent Bäertsch <vincentbaertsch@gmail.com>
2021-02-26 12:02:06 +0100

    lint: Add linter for code style

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+    svelteSortOrder: "scripts-styles-options-markup",
+    svelteStrictMode: false,
+    svelteBracketNewLine: true,
+    svelteAllowShorthand: false,
+    plugins: ["prettier-plugin-svelte"],
+    printWidth: 120,
+    jsxBracketSameLine: false,
+};
